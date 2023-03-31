029c145f09e0bc094dc535d5c272df3bbdcbc3f4
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-31 17:04:30 +0200

    add new message function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,16 @@
 // Milestone 2
 // Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 // Click sul contatto mostra la conversazione del contatto cliccato
-// Nota:
-// Se finite presto, richiedete la secoda milestone ai tutor oggi pomeriggio.
-
+// Milestone 3
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
+        newMessage:"",
         activeContact: 0,
         contacts: [
             {
@@ -182,6 +183,18 @@ const { createApp } = Vue
     methods: {
         clickOnContacts(index) {
             this.activeContact = index; 
+        }, 
+        sendMessage() {
+            if(this.newMessage!="") {
+                this.contacts[this.activeContact].messages.push(
+                    {
+                        date: 'newDate',
+                        message:this.newMessage, 
+                        status:'sent'
+                    }
+                )
+            } 
+
         }
     }
   }).mount('#app')
